5a12d558f6822f6865e725af5ca48bd2fd137274
Fuchao <alex@ozcoolpon.com.au>
2013-09-06 15:30:42 +1000

    First change
    dsfdsdf

---
+++ Slider/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Slider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program game = new Program();
+            //game start
+            if (args.Length != 0)
+                game.Run(args);
+            else
+            {
+                string s = Console.ReadLine();
+                string[] str;
+                str = s.Split();
+                while (game.Run(str))
+                {
+                    s = Console.ReadLine();
+                    str = s.Split();
+                }
+            }
+        }
+
+        public void SyntaxMsg()
+        {
+            string[] str={"Syntax : slider numrows numcols",
+                              "0 < numrows <= 9",
+                              "0 < numcols <= 9"};
+            //print the syntax message
+            foreach (string s in str)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        public void PrintMenu()
+        {
+            string[] str ={"m  : print this menu",
+                          "p  : print the puzzle",
+                          "nX : new puzzle. X is the puzzle seed",
+                          "rX : shift row X right",
+                          "cX : shift col X down",
+                          "x  : exit the program"};
+            //print the menu
+            foreach (string s in str)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+        }
+
+        public void PrintPuzzle(Puzzle p)
+        {
+            //print the puzzle
+            for (int i = 0; i < p.get_rows(); i++)
+            {
+                Console.Write("{0,-2}|", i);
+                for (int j = 0; j < p.get_cols(); j++)
+                {
+                    Console.Write("{0,3}", p.get_grid()[i, j]);
+                }
+                Console.WriteLine();
+                if (i == p.get_rows() - 1)
+                {
+                    Console.Write("--+");
+                    for (int k = 0; k < p.get_cols(); k++)
+                    {
+                        Console.Write("---");
+                    }
+                    Console.WriteLine();
+                    Console.Write("  |");
+                    for (int l = 0; l < p.get_cols(); l++)
+                    {
+                        Console.Write("{0,3}", l);
+                    }
+                    Console.WriteLine("\n");
+                }
+            }
+           
+
+        }
+
+        public bool Run(string[] mStr)
+        {
+            int rows, cols;
+            if (mStr.Count() != 2)
+            {
+                this.SyntaxMsg();
+                return true;
+            }
+            else
+            {
+                rows = int.Parse(mStr[0]);
+                cols = int.Parse(mStr[1]);
+                if (rows <= 0 || rows > 9 || cols <= 0 || rows > 9)
+                {
+                    this.SyntaxMsg();
+                    return true;
+                }
+            }
+            Puzzle puz = new Puzzle(rows, cols);
+            int x, suff = 0;
+            char ch;
+            string s;
+            Console.WriteLine("\nSlider Start.\n");
+            this.PrintMenu();
+            this.PrintPuzzle(puz);
+            //input command
+            while (true)
+            {
+                Console.Write("Enter command => ");
+                ch = (char)Console.Read();
+                s = Console.ReadLine();
+                int.TryParse(s, out x);
+                switch (ch)
+                {
+                    case'm':
+                        if (s.Length != 0)
+                            goto default;
+                        this.PrintMenu();
+                        break;
+                    case'p':
+                        if (s.Length != 0)
+                            goto default;
+                        this.PrintPuzzle(puz);
+                        break;
+                    case'n':
+                        puz.Shuffle(x);
+                        suff = 1;
+                        this.PrintPuzzle(puz);
+                        break;
+                    case'r':
+                        if (x < 0 || x >= rows)
+                            goto default;
+                        puz.MoveRowRight(x);
+                        suff = 1;
+                        this.PrintPuzzle(puz);
+                        break;
+                    case'c':
+                        if (x < 0 || x >= cols)
+                            goto default;
+                        puz.MoveColDown(x);
+                        suff = 1;
+                        this.PrintPuzzle(puz);
+                        break;
+                    case'x':
+                        return false;
+                    default:
+                        Console.WriteLine("Invalid command entered.");
+                        this.PrintMenu();
+                        continue;
+                }
+                //when solved,print the message
+                if (puz.isSolved()&&suff==1)
+                {
+                    Console.WriteLine("CONGRATULATIONS.You have solved the puzzle.");
+                    suff = 0;
+                }
+            }
+        }
+    }
+}
---
+++ Slider/Puzzle.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Slider
+{
+    class Puzzle
+    {
+        private int numRows, numCols;
+        private int[,] numGrid = new int[10, 10];
+        private int[,] orgGrid = new int[10, 10];
+
+        public Puzzle() { }
+        //initialize
+        public Puzzle(int rows, int cols)
+        {
+            numRows = rows;
+            numCols = cols;
+
+            for (int i = 0, value = 1; i < numRows; i++)
+            {
+                for (int j = 0; j < numCols; j++, value++)
+                {
+                    numGrid[i, j] = value;
+                    orgGrid[i, j] = value;
+                }
+            }        
+        }
+        public int get_rows()
+        {
+            return numRows;
+        }
+
+        public int get_cols()
+        {
+            return numCols;
+        }
+
+        public int[,] get_grid()
+        {
+            return numGrid;
+        }
+
+        public void MoveRowRight(int moveRow)
+        {
+            int colIndex = numCols - 1;
+            int temp = numGrid[moveRow, colIndex];
+            //move right
+            while (colIndex > 0)
+            {
+                numGrid[moveRow, colIndex] = numGrid[moveRow, --colIndex];
+            }
+            numGrid[moveRow, colIndex] = temp;
+        }
+
+        public void MoveColDown(int moveCol)
+        {
+            int rowIndex = numRows - 1;
+            int temp = numGrid[rowIndex, moveCol];
+            //move down
+            while (rowIndex > 0)
+            {
+                numGrid[rowIndex, moveCol] = numGrid[--rowIndex, moveCol];
+            }
+            numGrid[rowIndex, moveCol] = temp;
+        }
+
+        public void Shuffle(int seed)
+        {
+            int numMoves = numRows * numCols;
+            int numDir = numRows + numCols;
+            int direction, randRow, randCol;
+            Random r = new Random(seed);
+            for (int i = 0; i < numMoves; i++)
+            {
+                // randomly select whether we move a row or a column
+                direction = r.Next() % numDir;
+                if (direction < numRows)
+                {
+                    // move a randomly selected row right
+                    randRow = r.Next() % numRows;
+                    MoveRowRight(randRow);
+                }
+                else
+                {
+                    // move a randomly selected column down
+                    randCol = r.Next() % numCols;
+                    MoveColDown(randCol);
+                }
+            }
+        }
+
+        public bool isSolved()
+        {
+            for (int i = 0; i < numRows; i++)
+            {
+                for (int j = 0; j < numCols; j++)
+                {
+                    if (numGrid[i, j] != orgGrid[i, j])
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
